936bbe17410183161f26fab6ea9b4f73e1a0ddef
Simon George <S.George@rhul.ac.uk>
2021-04-05 22:27:03 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,39 @@
-fetch('https://environment.data.gov.uk/flood-monitoring/id/stations/417417TP/readings.json?parameter=rainfall&today&_limit=1000')
+fetch('https://environment.data.gov.uk/flood-monitoring/id/stations/417417TP/readings.json?parameter=rainfall&today&_limit=10000')
         .then(function (response) {
-                return response.text();
+                return response.json();
         })
-        .then(function (text) {
-                let series = text;
-                //console.log(series.items);
-                renderChart(series.items);
+        .then(function (json) {
+            let series = processData(json);
+            //console.log(series);
+            renderChart(series);
         })
         .catch(function (error) {
-                //Something went wrong
-                console.log(error);
+            //Something went wrong
+            console.log(error);
         });
 
+function processData(json) {
+    let data = [];
+    console.log(json);
+    json.items.forEach(function (row) {
+        //add either to Black, White arrays, or discard.
+        data.push({x: row.dateTime, y: row.value});
+    });
+    console.log(data);
+    return [
+        {name: 'rainfall(mm)', points: data},
+    ];
+}
 
+    
 function renderChart(series) {
     JSC.Chart('chartDiv', {
 	type: "line",
         series: series,
-	parsing: {
-	    xAxisKey: 'dateTime',
-	    yAxiskey: 'value'
-	},
+	//parsing: {
+	//    xAxisKey: 'dateTime',
+	//    yAxiskey: 'value'
+	//},
 	xAxis_scale_type: "time",
     });
 }
